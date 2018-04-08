a815209227f490c20db2f666542fc8f59a239fab
Riley Flynn <riley@rileyflynn.me>
2018-04-07 23:06:17 -0230

    Added a bunch of stuff that I honestly can't remember because I spent like the last 40 minutes tracking down a bug

--- jsonscript/interpreter.py
+++ jsonscript/interpreter.py
@@ -6,49 +6,102 @@ import requests
 
 class Interpreter(object):
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.variables: Dict[str, object] = {}
         self.methods: Dict[str, object] = {}
         self.proxy_methods: Dict[str, callable] = {}
         self._register_proxies()
 
-    def _register_proxies(self):
+    def _register_proxies(self) -> None:
+        """
+        Registers all JSON to Python proxy methods
+        """
         self.proxy_methods["set"] = self.set_value
+        self.proxy_methods["add"] = self.add
 
-    def run_file(self, path: str):
+    def run_file(self, path: str) -> None:
+        """
+        Loads a file and runs it
+        :param path: The path to the file
+        """
         with open(path, encoding="utf8") as f:
             statements = json.load(f)
             self.execute_statements(statements)
 
-    def run_url(self, url: str):
+    def run_url(self, url: str) -> None:
+        """
+        Loads a URL and runs it
+        :param url: The URL to load
+        """
         resp = requests.get(url)
         self.execute_statements(resp.json())
 
-    def set_value(self, name: str, value):
+    def process_value(self, value, arguments):
+        """
+        Processes a value. Executes it if it is a statement, otherwise returns the value
+        :param value: The value to process
+        :param arguments: The current arguments
+        :return: The processed value
+        """
+        # Execute the value if it is a statement
         if isinstance(value, dict):
-            self.execute_statements([value])
-            self.variables[name] = self.variables.get("RETVAL", None)
-        else:
-            self.variables[name] = value
+            self.execute_statements([value], arguments)
+            return self.variables.get("RETVAL", None)
+        # Otherwise, return the value
+        return value
 
-    def execute_statements(self, statements: Union[list, object], parameters=None):
-        if parameters is None:
-            parameters = {}
+    def set_value(self, args) -> None:
+        """
+        Sets a variable to a given value
+        :param name: The name of the variable to set
+        :param value: The new value
+        """
+        self.variables[args["name"]] = self.process_value(args["new_value"], args)
+
+    def add(self, args):
+        """
+        Adds two values together
+        :return: The result of adding the two values
+        """
+        l = self.process_value(args["left"], args)
+        r = self.process_value(args["right"], args)
+        return l + r
+
+    def execute_statements(self, statements: Union[list, object], arguments=None) -> None:
+        """
+        Executes a list of statements
+        :param statements: The list of statements to execute
+        :param arguments: A dict of arguments to be used, if this is a method call
+        """
+        if arguments is None:
+            arguments = {}
+
         for statement in statements:
+
+            for key in arguments:
+                if key not in statement.get("args", {}) and key not in self.variables:
+                    statement.get("args", {})[key] = arguments[key]
+
             if statement["operation"] == "define":
+                # If the user is attempting to define a method, add this statement's code list to the methods dict
                 name = statement["args"]["name"]
                 code = statement["args"]["code"]
                 self.methods[name] = code
 
             elif statement["operation"] == "get":
+                # Sets the return value to the value of a variable
                 name = statement["args"]["name"]
-                if name in parameters:
-                    self.variables["RETVAL"] = parameters[name]
-                else:
-                    self.variables["RETVAL"] = self.variables.get(name, None)
+                self.variables["RETVAL"] = self.variables.get(name, None)
 
+            elif statement["operation"] == "getarg":
+                self.variables["RETVAL"] = arguments[statement["args"]["name"]]
+
+            # If the operation is a method that has been declared, execute that method
             elif statement["operation"] in self.methods:
-                self.execute_statements(self.methods[statement["operation"]], statement.get("args", {}))
+                self.execute_statements(self.methods[statement["operation"]], statement["args"])
 
+            # If the operation is a proxy method that has been declared, call that method
             elif statement["operation"] in self.proxy_methods:
-                self.proxy_methods[statement["operation"]](**statement.get("args", {}))
+                result = self.proxy_methods[statement["operation"]](statement["args"])
+                if result is not None:
+                    self.variables["RETVAL"] = result
